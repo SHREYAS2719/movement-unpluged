172d8656f2d2d57bdf5813b4ca33c62d6a846cf0
SHREYAS2719 <146858211+SHREYAS2719@users.noreply.github.com>
2024-10-06 16:10:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+alert("welcome to our wesite..")
+var a = document.querySelector("h1")
+
+a.innerHTML="website comming soon !"
+
+a.style.color="yellow"
+
+a.addEventListener(click ,function(){
+
+})
